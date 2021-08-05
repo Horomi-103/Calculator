fb307152806025afc024fde15379fd1a63be35d5
Ksyu <666fog666@gmail.com>
2021-08-05 04:13:42 +0300

    Переделала отображение и запись нажатий пользователя под массив

--- calculation.js
+++ calculation.js
@@ -1,37 +1,51 @@
-var curResult = 0;
-var newNum = false;
-var pressValue = "";
+var list = [];
+var lastNum = list.pop();
 
-function press(inpNum) {
-    if (newNum) {
-        document.getElementById('resOut').value = inpNum;
-        newNum = false;
-
-    }
-    else {
-        if (document.getElementById('resOut').value == "0")
-            document.getElementById('resOut').value = inpNum;
-        else
-            document.getElementById('resOut').value += inpNum;
-    }
+function press(num) {
+	if(typeof lastNum == "number" && typeof num == "number"){
+ 		var changeNum = String(lastNum);
+    changeNum = changeNum + String(num);
+    lastNum = Number(changeNum);
+    list.pop();
+    list.push(lastNum);
+    console.log("я в первом if "+lastNum);
+    console.log(list);
+  }
+  else if(typeof num == "string" && typeof lastNum == "number"){
+  	lastNum = num;
+    list.push(lastNum);
+    console.log("я добавил операнд "+num);
+    console.log(list);
+  }
+  else if(typeof num == "number" && typeof lastNum == "string"){
+  	lastNum = num;
+    list.push(lastNum);
+    console.log("я добавил новое число "+num);
+    console.log(list);
+  }
+  else if(typeof num == "string" && typeof lastNum == "string"){
+  	lastNum = num;
+    list.pop();
+    list.push(lastNum);
+    console.log("я заменил операнд на "+num);
+    console.log(list);
+  }
+  else if(lastNum == undefined && typeof num == "string"){
+  //console.log('тыкнули на операнд')
+  }
+  else {//если пусто, добавляет число
+  	lastNum = num;
+    list.push(lastNum);
+    console.log("я в последнем else "+lastNum);
+    console.log(list);
+  }
+  var str = String(list);
+  str=str.replace(/[\/_,\\]/g, '');
+  document.querySelector(".inpPress").innerHTML = str;
 }
 
-function operation(oper) {
-    var result = document.getElementById('resOut').value;
-    if (newNum && pressValue != "=") {
-        document.getElementById('resOut').value = curResult;
-    }
-    else {
-        newNum = true;
-        if ('+' == pressValue) curResult += parseFloat(result);
-        else if ('-' == pressValue) curResult -= parseFloat(result);
-        else if ('/' == pressValue) curResult /= parseFloat(result);
-        else if ('*' == pressValue) curResult *= parseFloat(result);
-        else curResult = parseFloat(result);
-        
-        document.getElementById('resOut').value = curResult;
-        pressValue = oper;
-    }
+function calcResult(oper) {
+	console.log(list);
 }
 
 function clearAll() {
@@ -39,4 +53,5 @@ function clearAll() {
     curResult = 0;
     newNum = true;
     document.getElementById('resOut').value = "0";
+    document.querySelector(".inpPress").innerHTML = "";
 }
