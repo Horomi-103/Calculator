8698a2a8e371c0c804ca7248bcc9fa1e256ba983
Ksyu <666fog666@gmail.com>
2021-08-05 15:02:20 +0300

    1. Сделала 2 функции, убрала лишние проверки 2. Сделала поиск последнего элемента через [list.lenght-1]

--- calculation.js
+++ calculation.js
@@ -1,57 +1,54 @@
 var list = [];
-var lastNum = list.pop();
+var lastNum = list[list.length - 1];
 
-function press(num) {
-	if(typeof lastNum == "number" && typeof num == "number"){
- 		var changeNum = String(lastNum);
+function pressNum(num) {
+  if (typeof lastNum == "number") {
+    var changeNum = String(lastNum);
     changeNum = changeNum + String(num);
     lastNum = Number(changeNum);
     list.pop();
     list.push(lastNum);
-    console.log("я в первом if "+lastNum);
+    console.log("я в первом if " + lastNum);
     console.log(list);
   }
-  else if(typeof num == "string" && typeof lastNum == "number"){
-  	lastNum = num;
+  else {
+    lastNum = num;
     list.push(lastNum);
-    console.log("я добавил операнд "+num);
-    console.log(list);
-  }
-  else if(typeof num == "number" && typeof lastNum == "string"){
-  	lastNum = num;
-    list.push(lastNum);
-    console.log("я добавил новое число "+num);
-    console.log(list);
-  }
-  else if(typeof num == "string" && typeof lastNum == "string"){
-  	lastNum = num;
-    list.pop();
-    list.push(lastNum);
-    console.log("я заменил операнд на "+num);
-    console.log(list);
-  }
-  else if(lastNum == undefined && typeof num == "string"){
-  //console.log('тыкнули на операнд')
-  }
-  else {//если пусто, добавляет число
-  	lastNum = num;
-    list.push(lastNum);
-    console.log("я в последнем else "+lastNum);
+    console.log("я в последнем else " + lastNum);
     console.log(list);
   }
   var str = String(list);
-  str=str.replace(/[\/_,\\]/g, '');
+  str = str.replace(/[\_,\\]/g, '');
+  document.querySelector(".inpPress").innerHTML = str;
+}
+
+function pressOper(oper) {
+  if (typeof lastNum == "number") {
+    lastNum = oper;
+    list.push(lastNum);
+    console.log("я добавил операнд " + oper);
+    console.log(list);
+  }
+  else {
+    lastNum = oper;
+    list.pop();
+    list.push(lastNum);
+    console.log("я заменил операнд на " + oper);
+    console.log(list);
+  }
+  var str = String(list);
+  str = str.replace(/[\_,\\]/g, '');
   document.querySelector(".inpPress").innerHTML = str;
 }
 
 function calcResult(oper) {
-	console.log(list);
+  console.log(list);
 }
 
 function clearAll() {
-    pressValue = "";
-    curResult = 0;
-    newNum = true;
-    document.getElementById('resOut').value = "0";
-    document.querySelector(".inpPress").innerHTML = "";
+  pressValue = "";
+  curResult = 0;
+  newNum = true;
+  document.getElementById('resOut').value = "0";
+  document.querySelector(".inpPress").innerHTML = "";
 }
