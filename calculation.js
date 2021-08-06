6e5798deec4304f3e99cd504c1be1c637c656be3
Ksyu <666fog666@gmail.com>
2021-08-06 03:55:07 +0300

    Убрала выводы в консоль

--- calculation.js
+++ calculation.js
@@ -8,14 +8,14 @@ function pressNum(num) {
     lastNum = Number(changeNum);
     list.pop();
     list.push(lastNum);
-    console.log("я в первом if " + lastNum);
-    console.log(list);
+    //console.log("я в первом if " + lastNum);
+    //console.log(list);
   }
   else {
     lastNum = num;
     list.push(lastNum);
-    console.log("я в последнем else " + lastNum);
-    console.log(list);
+    //console.log("я в последнем else " + lastNum);
+    //console.log(list);
   }
   var str = String(list);
   str = str.replace(/[\_,\\]/g, '');
@@ -26,31 +26,31 @@ function pressOper(oper) {
   if (typeof lastNum == "number") {
     lastNum = oper;
     list.push(lastNum);
-    console.log("я добавил операнд " + oper);
-    console.log(list);
+    //console.log("я добавил операнд " + oper);
+    //console.log(list);
   }
   else {
     lastNum = oper;
     list.pop();
     list.push(lastNum);
-    console.log("я заменил операнд на " + oper);
-    console.log(list);
+    //console.log("я заменил операнд на " + oper);
+    //console.log(list);
   }
   var str = String(list);
   str = str.replace(/[\_,\\]/g, '');
   document.querySelector(".inpPress").innerHTML = str;
 }
 
-function calcResult(oper) {
-  console.log(list);
-}
-
 function clearAll() {
   pressValue = "";
   curResult = 0;
-  newNum = true;
   document.getElementById('resOut').value = "0";
   document.querySelector(".inpPress").innerHTML = "";
+  list.splice(0, list.length);
+
+  console.log(pressValue);
+  console.log(curResult);
+  console.log(list);
 }
 
 function calcResult() {
